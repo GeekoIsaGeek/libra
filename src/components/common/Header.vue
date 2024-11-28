28610c977e5b8a9b53211192f1c9f54d23c1a987
geekoisageek <george.kvrivi@gmail.com>
2024-11-28 23:13:04 +0400

    chore: move shared components into common dir

---
+++ src/components/common/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LanguageSwitcher from '@/components/common/LanguageSwitcher.vue';
+import SearchBar from '@/components/common/SearchBar.vue';
+</script>
+
+<template>
+	<header class="mt-24 w-[calc(100%-160px)] mx-20 px-8 py-6 rounded-lg bg-black/80 border border-gray-400 shadow-md">
+		<div class="flex items-center justify-between">
+			<SearchBar />
+			<LanguageSwitcher />
+		</div>
+	</header>
+</template>
---
+++ src/components/common/SearchBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SearchIcon from '@/components/icons/SearchIcon.vue';
+import FiltersIcon from '@/components/icons/FiltersIcon.vue';
+</script>
+
+<template>
+	<div class="px-4 py-2 flex items-center gap-2 bg-white border border-gray-500 w-[60%] shadow rounded-full">
+		<SearchIcon class="icon" />
+		<input
+			type="text"
+			class="w-full text-black outline-none placeholder:text-sm"
+			:placeholder="$t('search.inputPlaceholder') + '...'"
+		/>
+		<FiltersIcon class="icon" />
+	</div>
+</template>
