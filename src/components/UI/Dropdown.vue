c503e1a5754bd5a338f938fec775758eba5e879b
geekoisageek <george.kvrivi@gmail.com>
2024-11-29 10:43:07 +0400

    refactor: make dropdown a reusable component

---
+++ src/components/UI/Dropdown.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+	options: {
+		type: Array,
+		required: true,
+	},
+	selectHandler: {
+		type: Function,
+		required: true,
+	},
+	showDropdown: {
+		type: Boolean,
+		required: true,
+	},
+});
+
+defineEmits(['update:showDropdown']);
+</script>
+
+<template>
+	<div class="relative">
+		<div
+			class="flex items-center justify-between gap-4 bg-white border border-gray-500 rounded-md px-2 py-1 text-black select-none cursor-pointer hover:bg-white/90 transition-colors duration-200 ease-out shadow w-[10rem]"
+			@click="$emit('update:showDropdown', !showDropdown)"
+		>
+			<slot />
+		</div>
+
+		<ul
+			class="absolute flex flex-col items-start w-full bg-white border border-gray-500 rounded-md text-black cursor-pointer shadow"
+			v-if="showDropdown"
+		>
+			<li
+				v-for="(option, i) in options"
+				:key="option"
+				@click="() => selectHandler(option)"
+				class="flex items-center gap-3 w-full px-2 hover:bg-[wheat]/50 transition-colors duration-200 ease-out py-1"
+			>
+				<slot name="option" :option="option" />
+			</li>
+		</ul>
+	</div>
+</template>
--- src/components/common/LanguageSwitcher.vue
+++ src/components/common/LanguageSwitcher.vue
@@ -5,42 +5,33 @@ import ArrowDownIcon from '@/components/icons/ArrowDownIcon.vue';
 import useLocale from '@/composables/useLocale';
 import { locales } from '@/config/localization';
 import { ref } from 'vue';
+import Dropdown from '@/components/UI/Dropdown.vue';
 
 const { setLocale, locale } = useLocale();
 const showDropdown = ref(false);
 
-const switchLanguage = (locale) => {
-	setLocale(locale);
+const switchLanguage = ([loc, lang]) => {
+	setLocale(loc);
 	showDropdown.value = false;
 };
 </script>
 
 <template>
-	<div class="relative">
-		<div
-			class="flex items-center justify-between gap-4 bg-white border border-gray-500 rounded-md px-2 py-1 text-black select-none cursor-pointer hover:bg-white/90 transition-colors duration-200 ease-out shadow w-[10rem]"
-			@click="showDropdown = !showDropdown"
-		>
+	<Dropdown
+		v-model:showDropdown="showDropdown"
+		:options="Object.entries(locales)"
+		:selectHandler="(selectedLocale) => switchLanguage(selectedLocale)"
+	>
+		<template #default>
 			<div class="flex items-center gap-3">
 				<img :src="locale === 'en' ? UK : GE" class="w-4 h-4" />{{ locales[locale] }}
 			</div>
 			<ArrowDownIcon class="icon" />
-		</div>
+		</template>
 
-		<ul
-			class="absolute flex flex-col items-start w-full bg-white border border-gray-500 rounded-md text-black cursor-pointer shadow"
-			v-if="showDropdown"
-		>
-			<li
-				v-for="([loc, lang], i) in Object.entries(locales)"
-				:key="loc"
-				@click="switchLanguage(loc)"
-				class="flex items-center gap-3 w-full px-2 hover:bg-[wheat]/30 transition-colors duration-200 ease-out py-1"
-				:class="{ 'bg-[wheat]/80': loc === locale }"
-			>
-				<img :src="loc === 'en' ? UK : GE" alt="country-flag" class="w-4 h-4" />
-				{{ lang }}
-			</li>
-		</ul>
-	</div>
+		<template v-slot:option="{ option: [loc, lang] }">
+			<img :src="loc === 'en' ? UK : GE" alt="country-flag" class="w-4 h-4" />
+			{{ lang }}
+		</template>
+	</Dropdown>
 </template>
